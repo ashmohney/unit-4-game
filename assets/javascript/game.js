e5793d9e671b42ea1565ef0e1aa737dac0f9c349
Ash Mohney <44374838+ashmohney@users.noreply.github.com>
2019-01-07 14:02:35 -0500

    initial doc set-up

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+/* json objects for each character:
+    name, health points, attack points, 
+four movable buttons that can disappear
+
+if/else 
+
+Math.floor and math.random 
+        need to figure out how things add and subtract; do we multiply
+
+
+
+
+        ---------
+        theCobbWeb.biggestWeb,biggerWeb.get(0) 
+
+        for (i=0; 
+
+
+
+
+
+
+
+
+*/
