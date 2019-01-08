280aa35827daae85c9ce7eb8cadec11d4f38b465
Ash Mohney <44374838+ashmohney@users.noreply.github.com>
2019-01-08 15:20:30 -0500

    html factored and psuedo-coded js. Ready for jQuery.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,42 @@
-/* json objects for each character:
+/* 
+
+Attack and Defend Game:
+
+Four characters
+User chooses character to fight and then to fight with (Defender). 
+User clicks Attack button:
+    Attacker wins/loses points
+    Defender wins/loses points.
+
+Health Points and Attack points reflect ups and downs. 
+
+If Attacker wins:
+    that character gains higher Attack-Points power. 
+    User chooses another chacter to Attack. 
+
+If Attakcer loses: 
+    "You lose" --> maybe not as an alert (put in div?)
+    Restart button appears
+
+
+JSON objects for each character:
     name, health points, attack points, 
-four movable buttons that can disappear
 
-if/else 
+four movable buttons that can disappear and reappear in appropriate div (cards?)
 
-Math.floor and math.random 
-        need to figure out how things add and subtract; do we multiply
+if/else loops or switches?? 
 
-
-
-
-        ---------
-        theCobbWeb.biggestWeb,biggerWeb.get(0) 
-
-        for (i=0; 
-
-
-
-
-
-
-
-
+Math.floor and math.random  --> maybe not rando numbers, will need set values. 
+  
 */
+$(document).ready (function() {
+
+
+
+
+
+
+
+    
+
+});
