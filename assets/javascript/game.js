603fc17f909c5508a24d9b4cc34c67a0ed8711fd
Ash Mohney <44374838+ashmohney@users.noreply.github.com>
2019-01-09 10:25:52 -0500

    Chacking if local vars get read with update to github

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,23 @@
 Attack and Defend Game:
 
 Four characters
-User chooses character to fight and then to fight with (Defender). 
+User chooses/clicks character for attack 
+        card moves to attacker-area
+        text to ask user to choose defender pops into defender-area
+then user clicks character as defender
+        card moves to defender-area
+        Attack button appears
 User clicks Attack button:
     Attacker wins/loses points
-    Defender wins/loses points.
+    Defender wins/loses points
+    all documented in the score-area
 
 Health Points and Attack points reflect ups and downs. 
 
 If Attacker wins:
     that character gains higher Attack-Points power. 
-    User chooses another chacter to Attack. 
+    User chooses another character as defender
+    Restart button appears
 
 If Attakcer loses: 
     "You lose" --> maybe not as an alert (put in div?)
@@ -22,12 +29,13 @@ If Attakcer loses:
 JSON objects for each character:
     name, health points, attack points, base points counter attack, etc. 
 
-four movable buttons that can disappear and reappear in appropriate div (cards?)
 
 if/else loops or switches?? 
   
 */
 $(document).ready (function() {
+
+
 
 //Setting Global Vars
 
@@ -47,74 +55,61 @@ var defender = null;
 
 //Chacter Objects at start function:
 
-    characters = [
-       
-            {
-                id: 0,
-                name: "Han Solo",
-                healthPoints: 120,
-                baseAttack:10,
-                attackPower: 10,
-                counterAttackPower: 8,
-                img:"assets/images/Han_Solo.jpg",
-                value: "soloHans",
-            },
+        function startGame() {   
+                hanSolo = {
+                        id: 0,
+                        name: "Han Solo",
+                        healthPoints: 120,
+                        baseAttack:10,
+                        attackPower: 10,    
+                        counterAttackPower: 8,
+                        img:"assets/images/Han_Solo.jpg",
+                        value: "soloHans",
+                    },
 
-            {
-                id: 1,
-                name: "Carl",
-                healthPoints: 100,
-                baseAttack: 8,
-                attackPower: 8,
-                counterAttackPower: 5,
-                img:"assets/images/storm-trooper.jpg",
-                value: "trooperCarl",
-            },
+                stormTrooper = {
+                        id: 1,
+                        name: "Carl",
+                        healthPoints: 100,
+                        baseAttack: 8,
+                        attackPower: 8,
+                        counterAttackPower: 5,
+                        img:"assets/images/storm-trooper.jpg",
+                        value: "trooperCarl",
+                    },
 
-            {
-                id: 2,
-                name: "Chewy",
-                healthPoints: 150,
-                baseAttack:9,
-                attackPower: 9,
-                counterAttackPower: 10,
-                img:"assets/images/chewy.jpg",
-                value: "baccaChew",
-            },
+                chewBacca = {
+                        id: 2,
+                        name: "Chewy",
+                        healthPoints: 150,
+                        baseAttack:9,
+                        attackPower: 9,
+                        counterAttackPower: 10,
+                        img:"assets/images/chewy.jpg",
+                        value: "baccaChew",
+                    },
 
-            {
-                id: 3,
-                name: "Darth Vader",
-                healthPoints: 100,
-                baseAttack: 12,
-                attackPower: 12,
-                counterAttackPower: 12,
-                img:"assets/images/DarthVader.jpg",
-                value: "vaderDarth",
-            },
-        ];
+                darthVader = {
+                        id: 3,
+                        name: "Darth Vader",
+                        healthPoints: 100,
+                        baseAttack: 12,
+                        attackPower: 12,
+                        counterAttackPower: 12,
+                        img:"assets/images/DarthVader.jpg",
+                        value: "vaderDarth",
+                    },
+                
 
-    
-    
-    $("#solo").on("click", function(event) {
-        console.log("Han Solo!");
+            
+            
+            $(".xcharacter").on("click", function(event) {
+                console.log("WTF");
 
-    });
-
-    $("#trooper").on("click", function(event) {
-        console.log("Carl!");
-        
-    });
-
-    $("#bacca").on("click", function(event) {
-        console.log("Chewbacca!");
-        
-    });
-
-    $("#vader").on("click", function(event) {
-        console.log("I am your father.");
-        
-    });
+            });
+        };
+    startGame();
+            
 
 
 
